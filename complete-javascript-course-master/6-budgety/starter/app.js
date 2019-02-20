fc1915da01245df1baf41abfd36594d22b3089bf
David McEldowney <dmmceldowney@gmail.com>
2019-02-20 08:38:24 -0600

    added a bit to the ctrlAddItem class instantiation

--- complete-javascript-course-master/6-budgety/starter/app.js
+++ complete-javascript-course-master/6-budgety/starter/app.js
@@ -27,16 +27,28 @@ var budgetController = (function(){
         addItem: function(type, des, val){            
             var newItem, ID;
             
-            ID = allItems[type][data.allItems[type].length()-1].id;
+            // create new ID
+            if (data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
 
+                // Create new item based on 'inc' or 'exp' type
             if (type === 'exp'){
                 newItem = new Expense(ID, des, val);
             } else if (type === 'inc') {
                 newItem = new Income(ID, des, val);
             }
-            
+            // push it to the data structure
             data.allItems[type].push(newItem);
+
+            // return the new element
             return newItem;
+        },
+        testing: function(){
+            console.log(data);
         }
     };
 
@@ -88,15 +100,20 @@ var controller = (function(budgetCtrl, UICtrl){
     
 
     var ctrlAddItem = function(){
-
+        var input, newItem;
+        
         // 1. get field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
+        
         // 2. Add item to the budget controller
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        
         // 3. Add the new item to the UI
 
+        
         // 4. Calculate budget
 
+        
         // 5. Display budget on UI
     };
 
